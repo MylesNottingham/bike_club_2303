89cce82b9c827202f13400c22b99b7a09e35db60
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-25 09:27:23 -0600

    Feat: Create Ride class.

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,20 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain
 
+  def initialize(ride_info)
+    @name = ride_info[:name]
+    @distance = ride_info[:distance]
+    @terrain = ride_info[:terrain]
+    @is_loop = ride_info[:loop]
+  end
+
+  def loop?
+    @is_loop
+  end
+
+  def total_distance
+    loop? ? @distance : @distance * 2
+  end
 end
