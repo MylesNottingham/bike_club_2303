169196b1371f22abb59aedafeb01aa75935bb0a8
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-25 09:42:46 -0600

    Feat: Add log_ride method to biker_Spec.

--- lib/biker.rb
+++ lib/biker.rb
@@ -7,11 +7,15 @@ class Biker
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
-    @rides = {}
+    @rides = Hash.new([])
     @acceptable_terrain = []
   end
 
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
+  end
+
+  def log_ride(ride, time)
+    @rides[ride] += [time]
   end
 end
